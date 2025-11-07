2c3ad33ab8bf132d6d70e509cc0744ae4a109e9e
Bryan Prinz <146386080+Bryanps7@users.noreply.github.com>
2025-11-07 12:24:38 -0300

    v2025.11.07

--- assets/js/halloween.js
+++ assets/js/halloween.js
@@ -1,4 +1,13 @@
-const alfabeto = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const alfabeto = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z',
+    'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG',
+    'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN',
+    'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU',
+    'AV', 'AW', 'AX', 'AY', 'AZ'
+];
 const numeros = Array.from({ length: 20 }, (_, i) => i + 1);
 
 const participantes = [
